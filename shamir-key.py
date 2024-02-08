4474d82a598ad9b5e4d6606ee7358cef7abae4da
Braincoke24 <37015058+Braincoke24@users.noreply.github.com>
2024-02-08 15:23:22 +0100

    replace spaces with tabs

--- shamir-key.py
+++ shamir-key.py
@@ -3,20 +3,20 @@ BASE_DICT = dict((c, v) for v, c in enumerate(BASE_ALPH))
 BASE_LEN = len(BASE_ALPH)
 
 def base_decode(string):
-    num = 0
-    for char in string:
-        num = num * BASE_LEN + BASE_DICT[char]
-    return num
+	num = 0
+	for char in string:
+		num = num * BASE_LEN + BASE_DICT[char]
+	return num
 
 def base_encode(num):
-    if not num:
-        return BASE_ALPH[0]
+	if not num:
+		return BASE_ALPH[0]
 
-    encoding = ''
-    while num:
-        num, rem = divmod(num, BASE_LEN)
-        encoding = BASE_ALPH[rem] + encoding
-    return encoding
+	encoding = ''
+	while num:
+		num, rem = divmod(num, BASE_LEN)
+		encoding = BASE_ALPH[rem] + encoding
+	return encoding
 
 with open('prime','r') as f:
 	p = int(f.read())
